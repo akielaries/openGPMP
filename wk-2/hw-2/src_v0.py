b1174ef907b53eca46308968ee3cf1ae08baf005
montaquila <akiel@akiel.org>
2022-09-09 22:18:38 -0700

    adding updates still broken

--- wk-2/hw-2/src_v0.py
+++ wk-2/hw-2/src_v0.py
@@ -15,7 +15,7 @@ from sklearn.preprocessing import StandardScaler #
 from sklearn.linear_model import LogisticRegression
 
 # our src files we want to download. spam and test sets
-test_url = 'https://github.com/tdhock/cs499-599-fall-2022/tree/main/data/zip.test.gz'
+test_url = 'https://github.com/tdhock/cs570-spring-2022/raw/master/data/zip.test.gz'
 test_file = 'zip.test.gz'
 spam_url = 'https://github.com/tdhock/cs499-599-fall-2022/tree/main/data/spam.data'
 spam_file = 'spam.data'
@@ -26,7 +26,6 @@ test_cols = 257
 spam_cols = 57
 # dataframe list
 test_acc_df_list = []
-
 
 """ 
 our last assignment was only downloading 1 file, to adhere to guidelines and 
@@ -39,7 +38,7 @@ def retrieve(src_file, src_url):
     """
     if not os.path.isfile(src_file):
         urllib.request.urlretrieve(src_url, src_file)
-        print("Downloading " + src_file + " from" + src_url + "...\n")
+        print("Downloading " + src_file + " from " + src_url + "...\n")
 
     else:
         print(src_file + " already exists in this folder...continuing anyway\n")
@@ -53,49 +52,62 @@ this method will
     - drop specified rows of the src file
     - convert our data into numpy 
 Hopefully this is allowed!
+    notice: (am repeating code) but spent too much time creating traveral
+    type solution going over a list of src_files and src_urls.
+    Wanted to seperate file into some seperate functions for the sake of 
+    readability.
 """
-#def init_df(src):
-    # dict for our source files
-    #src_dict = {}
-    # create empty list to store dataframes in
-
-    # traverse thru our src files
-    #for i in range(len(src)):
-        # read in downloaded src file as a pandas dataframe
-        # src[name] = pd.read_csv(src, header=None, sep=" ")
-        #df = pd.
-        # print name of our dataframe by traversing thru our dictionary
-        #for name, df in src_dict.items():
-        #    print(src_dict)
-
-    
+def df_init(src1, src2):#, spam_file):#, src1_cols, src2_cols):
     # read in downloaded src file as a pandas dataframe
-    # df2 = pd.read_csv(src2, header=None, sep=" ")
-    
-    # remove any rows which have non-01 labels
-    # df1[0] = df1[0].astype(int)
-    # df1 = df1[df1[0].isin([0, 1])]
-    # drop empty cols
-    # df1 = df1.drop(columns=[test_cols])
+    # seperate dataframes because different manipulations will be done
+    df1 = pd.read_csv(src1, header=None, sep=" ")
+    df2 = pd.read_csv(src2, header=None, sep='delimiter')
 
+    # remove any rows which have non-01 labels
+    df1[0] = df1[0].astype(int)
+    # df2[0] = df2[0].astype(int)
+    df1 = df1[df1[0].isin([0, 1])]
+    # df2 = df2[df2[0].isin([0, 1])]
+
+    # drop empty cols for specifiec rows 
+    #df1 = df1.drop(columns=[src1_cols])
+    #df2 = df2.drop(columns=[src2_cols])
+    
+    # initialize and declare vectors for our df columns
+    df1_label = df1[0]
+    df2_label = df2[spam_cols]
+    
+    """
+    convert dataframe to numpy array exlcuding the first column; iloc for row
+    and col specifying. Previous assignment moved from 1->end, this one will go
+    from 1-> the specified value from our column definitions
+    """
+    df1 = df1.iloc[:,1:257].to_numpy()
+    df2 = df2.iloc[:,1:57].to_numpy()
+    
+    print(df1)
+    print(df2)
+    
+"""
+an attempt to modularize our code to improve readibility, this method 
+will make a ggplot to visually examine which learning algorithm is 
+best for each data set
+"""
 # def plot():
 
 def main():
     # list of src files; we want to minimize reusing code
-    src_files = [test_file, spam_file]
+    #src_files = [test_file, spam_file]
     # list of src URLS
-    src_urls = [test_url, spam_url]
+    #src_urls = [test_url, spam_url]
     
     # retrieve our source files. Spam and zip. 
     retrieve(test_file, test_url)
     retrieve(spam_file, spam_url)
 
-    # call method to initialize our data frames 
-    #init_df(test_file, spam_file)
-    #init_df(src_files)
+    # call method to initialize our data frames, convert to numpy arrays, 
+    df_init(test_file, spam_file)#, spam_file)#, test_cols, spam_cols)
     
-    # initialize dictionary from our two dataframes
-    # init_dict()
 
 if __name__ == '__main__':
     # run main
