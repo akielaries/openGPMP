204743208865dd20b403bcc5158f96f177a9d6f3
montaquila <akiel@akiel.org>
2022-09-15 19:32:40 -0700

    adding file for hw 3

---
+++ wk-3/hw-3/src_v1.py
@@ -0,0 +1,40 @@
+# lib for retrieving src file from web
+import urllib.request
+# lib for reading files on OS
+import os
+# lib used for copying src file info into destination
+import shutil
+import pandas as pd
+import plotnine as p9
+import numpy as np
+# could not figure out how to calculate the mode of a list, using mode from lib
+import warnings # silence plethora of warning we get when running this
+from statistics import mode
+import sklearn
+from sklearn.model_selection import KFold #train/test splits
+from sklearn.model_selection import GridSearchCV #selecting best # of neighbors
+from sklearn.neighbors import KNeighborsClassifier #nearest_neighbors prediction.
+from sklearn.pipeline import make_pipeline # increase iteration sz
+from sklearn.preprocessing import StandardScaler # 
+from sklearn.linear_model import LogisticRegression
+
+# our src files we want to download. spam and test sets
+test_url = 'https://hastie.su.domains/ElemStatLearn/datasets/zip.test.gz'
+test_file = 'zip.test.gz'
+train_url = 'https://hastie.su.domains/ElemStatLearn/datasets/zip.train.gz'
+train_file = 'zip.train.gz'
+spam_url = 'https://hastie.su.domains/ElemStatLearn/datasets/spam.data'
+spam_file = 'spam.data'
+
+# number of columns in test file (257) count from zero
+conc_cols = 257 
+# number of columns in spam file (56) count from zero
+spam_cols = 57
+
+kf = KFold(n_splits=3, shuffle=True, random_state=1)
+
+# increase the max iteration from default 100
+pipe = make_pipeline(StandardScaler(), LogisticRegression(max_iter=1000))
+
+
+
