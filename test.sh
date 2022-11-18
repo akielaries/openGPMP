0bd73829ec2fc00a6a00553c0cf68127d4ce742e
montaquila <akiel@akiel.org>
2022-11-17 21:27:47 -0700

    updating the test run script. adding some fun ascii art as well :)

--- test.sh
+++ test.sh
@@ -1,6 +1,18 @@
 #! /bin/sh
-# script to compile and run our program. using just makefiles is a pain :(
+
+#   ____   __  __                _          
+#  |  _ \ |  \/  |        _ __  | | __ __ _ 
+#  | |_) || |\/| | _____ | '_ \ | |/ // _` |
+#  |  _ < | |  | ||_____|| |_) ||   <| (_| |
+#  |_| \_\|_|  |_|       | .__/ |_|\_\\__, |
+#                        | |           __| |
+#                        |_|          |___/ 
+# this script acts as the main "runner" for RM-pkg demonstrating its
+# functionality by running each modules driver file 
+
 # build + run tests in the makefile 
 make arith
 make lin-reg
+# delete generated binaries
+make clean
 
