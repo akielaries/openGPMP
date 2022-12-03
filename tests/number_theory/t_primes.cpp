c4a25adf4a869eefca4897a34c1960bd9019d02d
montaquila <akiel@akiel.org>
2022-12-03 16:50:06 -0700

    updating coverage with primes unit

--- tests/number_theory/t_primes.cpp
+++ tests/number_theory/t_primes.cpp
@@ -14,11 +14,16 @@ namespace {
 
     TEST(is_prime_test, verify) {
         int val_0 = 2;
-
         bool actual_0 = true;
         bool fetched_0 = p.is_prime(val_0);
         
         EXPECT_EQ(actual_0, fetched_0);
+
+        int val_1 = 3;
+        bool actual_1 = true;
+        bool fetched_1 = p.is_prime(val_1);
+        
+        EXPECT_EQ(actual_1, fetched_1);
     }
 }
 
