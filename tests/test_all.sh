08b5a27e6163c22b115850941ae7f23016d2c71f
montaquila <akiel@akiel.org>
2022-12-03 16:44:07 -0700

    updating coverage with primes unit

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -3,6 +3,7 @@
 # test all modules together with googletest. produce ONE .info coverage
 # file for the whole test suite. 
 g++ --coverage arith/t_arith.cpp ../modules/arithmetic/arith.cpp \
+    calc/t_calc.cpp ../modules/calculus/deriv.cpp \
     number_theory/t_cipher.cpp ../modules/number_theory/cipher.cpp \
     number_theory/t_rc4.cpp ../modules/number_theory/rc4.cpp \
     number_theory/t_primes.cpp ../modules/number_theory/primes.cpp \
@@ -18,5 +19,5 @@ lcov --directory . --capture --output-file lcov.info
 mv lcov.info ../.coverage
 
 # cd into root directory and run gcovr from here
-cd ../ && gcovr --xml-pretty >> codecov.xml && mv codecov.xml .coverage/
+cd ../ && gcovr --xml-pretty >> lcov.xml && mv lcov.xml .coverage/
 
