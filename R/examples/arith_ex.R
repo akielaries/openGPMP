98c46a5922723cb86cf2b5eedaa433184a874d18
montaquila <akiel@akiel.org>
2022-12-12 04:53:29 -0700

    updating R API. Will research how cpp classes were converted for R use

---
+++ R/examples/arith_ex.R
@@ -0,0 +1,9 @@
+# Example using RM-pkg's R API
+
+dyn.load(paste("../lib/RM_pkg", .Platform$dynlib.ext, sep=""))
+source("../lib/RM_pkg.R")
+
+out <- "Welcome to RM-pkg's R API."
+print(out)
+
+
--- R/lib/example.R
+++
@@ -1,8 +0,0 @@
-# Example using RM-pkg's R API
-
-dyn.load(paste("RM_pkg", .Platform$dynlib.ext, sep=""))
-source("RM_pkg.R")
-
-out <- "Welcome to RM-pkg's R API."
-print(out)
-
