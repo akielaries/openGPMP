0ee97961dc7645a516ea61c6069484b3a0cd5edb
Akiel Aries <56521583+akielaries@users.noreply.github.com>
2022-12-15 19:52:58 -0700

    Update naive_network.cpp

--- modules/ML_DL/naive_network.cpp
+++ modules/ML_DL/naive_network.cpp
@@ -1,7 +1,7 @@
 /*
  * Implementation of a standard or naive neural network
  */
-#include "../../include/linalg/matices.hpp"
+#include "../../include/linalg/matrices.hpp"
 #include "../../include/ML_DL/naive_network.hpp"
 
 
