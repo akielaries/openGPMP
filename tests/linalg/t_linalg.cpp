d9f1f50d1bfb3dea0260d16de05fd69640235f38
montaquila <akiel@akiel.org>
2022-12-18 14:13:32 -0700

    adding linear regression and linear algebra operations test files for development

---
+++ tests/linalg/t_linalg.cpp
@@ -0,0 +1,12 @@
+/*
+ * Testing Linear Algebra Operations
+ */
+#include "../../include/linalg/lao.hpp"
+#include <string>
+#include <limits.h>
+#include <gtest/gtest.h>
+
+using ::testing::InitGoogleTest;
+using ::testing::FloatLE;
+using ::testing::DoubleLE;
+
---
+++ tests/ml/t_linreg.cpp
@@ -0,0 +1,14 @@
+/*
+ * Testing Linear Regression
+ */
+#include "../../include/ml/linreg.hpp"
+#include <string>
+#include <limits.h>
+#include <gtest/gtest.h>
+
+using ::testing::InitGoogleTest;
+using ::testing::FloatLE;
+using ::testing::DoubleLE;
+
+
+
