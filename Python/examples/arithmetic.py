ecc886f5cbe3121fb5742198f1a87370bd8727c2
montaquila <akiel@akiel.org>
2022-12-30 16:16:59 -0700

    gitignore

--- Python/examples/arithmetic.py
+++ Python/examples/arithmetic.py
@@ -2,15 +2,21 @@
 # from Python import examples
 from openMTPK import Basics
 
-b = Basics()
 
-r1 = b.op_gcd(1703210, 20320)
-r2 = b.op_gcd(2198466, 96096)
-r3 = b.greatest_power(232, 96)
-r4 = b.greatest_power(10, 3)
+def main():
+    b = Basics()
 
-print(r1)
-print(r2)
-print(r3)
-print(r4)
+    r1 = b.op_gcd(1703210, 20320)
+    r2 = b.op_gcd(2198466, 96096)
+    r3 = b.greatest_power(232, 96)
+    r4 = b.greatest_power(10, 3)
+
+    print(r1)
+    print(r2)
+    print(r3)
+    print(r4)
+
+
+if __name__ == '__main__':
+    main()
 
--- Python/examples/cipher_ex.py
+++ Python/examples/cipher_ex.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3.10
 from openMTPK import Cipher
 
-print("Using the Number Theory module in Python")
-print("Methods of the Cipher class:\n\
-    - C_cipher(plaintext, key)\n\
-        : takes in plain text and key for shifting text\n\
-    \n\
-    - KC_encode(key)\n\
-        : pass in key to encode for corresponding algorithm\n\
-    - KC_cipher(plaintext, encoded_text)\n\
-        : recieves encoded key text, plaintext, returning cipher\n")
 
-c = Cipher()
-ciph = c.C_cipher('Plaintext', 5)
-print(ciph)
+    def main():
+    print("Using the Number Theory module in Python")
+    print("Methods of the Cipher class:\n\
+        - C_cipher(plaintext, key)\n\
+            : takes in plain text and key for shifting text\n\
+        \n\
+        - KC_encode(key)\n\
+            : pass in key to encode for corresponding algorithm\n\
+        - KC_cipher(plaintext, encoded_text)\n\
+            : recieves encoded key text, plaintext, returning cipher\n")
 
-ciph_1 = c.C_cipher('ATTACKATONCE', 4)
-print(ciph_1)
+    c = Cipher()
+    ciph = c.C_cipher('Plaintext', 5)
+    print(ciph)
 
-text = "Password"
-shift = "Computer"
-encoded_text = c.KC_encode(shift);
-hashtext = c.KC_cipher(text, encoded_text);
-print(hashtext)
+    ciph_1 = c.C_cipher('ATTACKATONCE', 4)
+    print(ciph_1)
+
+    text = "Password"
+    shift = "Computer"
+    encoded_text = c.KC_encode(shift);
+    hashtext = c.KC_cipher(text, encoded_text);
+    print(hashtext)
+
+
+if __name__ == '__main__':
+    main()
 
