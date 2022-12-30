af5230755b25428b5c01a06d098983d885d4180a
montaquila <akiel@akiel.org>
2022-12-30 01:55:12 -0700

    updating doxygen docs

--- docs/doxygen/html/search/all_0.js
+++ docs/doxygen/html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_3cimg_20src_3d_22https_3a_2f_2fgithub_2ecom_2fakielaries_2frm_2dpkg_2fblob_2fmain_2fdocs_2fopenmtpk_2esvg_22_20alt_3d_22img_22_2f_3e_0',['&lt;img src=&quot;https://github.com/akielaries/RM-pkg/blob/main/docs/openMTPK.svg&quot; alt=&quot;img&quot;/&gt;',['../index.html',1,'']]]
+  ['_3ca_20href_3d_22https_3a_2f_2fgithub_2ecom_2fakielaries_2frm_2dpkg_2fblob_2fmain_2fdocs_2fopenmtpk_2esvg_22_20_3eimg_3c_2fa_3e_0',['&lt;a href=&quot;https://github.com/akielaries/RM-pkg/blob/main/docs/openMTPK.svg&quot; &gt;img&lt;/a&gt;',['../index.html',1,'']]]
 ];
