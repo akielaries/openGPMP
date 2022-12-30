b713df4eaee23edf194adcf8c0e76f23997a09df
akielaries <56521583+akielaries@users.noreply.github.com>
2022-12-30 08:29:15 +0000

    Deploying to gh-pages from  @ 98dc401b4ca999b0a3395999aa46d968379557fe 🚀

--- search/files_0.js
+++ search/files_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_5f_5finit_5f_5f_2epy_0',['__init__.py',['../Python_2____init_____8py.html',1,'(Global Namespace)'],['../Python_2examples_2____init_____8py.html',1,'(Global Namespace)'],['../samples_2python_2____init_____8py.html',1,'(Global Namespace)']]]
+  ['readme_2emd_0',['README.md',['../README_8md.html',1,'']]]
 ];
