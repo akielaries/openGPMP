85b2e65eb92f178496b24c46f7872fbc721bc89b
montaquila <akiel@akiel.org>
2022-12-30 16:07:55 -0700

    updating python API

--- Python/examples/arithmetic.py
+++ Python/examples/arithmetic.py
@@ -4,8 +4,8 @@ from openMTPK import Basics
 
 b = Basics()
 
-r1 = b.rm_gcd(1703210, 20320)
-r2 = b.rm_gcd(2198466, 96096)
+r1 = b.op_gcd(1703210, 20320)
+r2 = b.op_gcd(2198466, 96096)
 r3 = b.greatest_power(232, 96)
 r4 = b.greatest_power(10, 3)
 
