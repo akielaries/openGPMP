e16a00b313d1c60bc5cbedb584ce03ae9c5a594c
montaquila <akiel@akiel.org>
2023-01-05 01:43:09 -0700

    testing Python PIP installation with PyPi

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ import subprocess
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
-subprocess.call(['make compile compile-wrapper compile-lib clean', 
-                 '-C',
-                 'Python/openmtpk'])
+#subprocess.call(['make compile compile-wrapper compile-lib clean', 
+#                 '-C',
+#                 'Python/openmtpk'])
 
 setuptools.setup(
     name='openmtpk',
