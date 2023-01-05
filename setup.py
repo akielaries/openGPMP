24fe1a6337ee4ff7721668ed6481835eec79b835
montaquila <akiel@akiel.org>
2023-01-05 13:25:11 -0700

    testing Python PIP installation with PyPi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 import subprocess
 
 
-with open('../../README.md', 'r', encoding='utf-8') as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 #subprocess.call(['make compile compile-wrapper compile-lib clean', 
