5cae203199e1d9dc432d7dcad5d50c472aaf646f
Akiel Aries <56521583+akielaries@users.noreply.github.com>
2023-01-05 15:53:52 -0700

    Creating Python pip package for Python API (#12)

--- samples/python/primes_ex.py
+++ samples/python/primes_ex.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3.10
-#from lib.openMTPK import Primality
-from ..lib import openMTPK
+#!/usr/bin/python3
 
-#from openMTPK import Primality
+
+#from lib.openMTPK import Primality
+
+from openmtpk import Primality
 
 
 def main():
