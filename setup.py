1ba35fef7da191aa6a65fb386803395fd7016035
montaquila <akiel@akiel.org>
2023-01-05 15:56:33 -0700

    updating PyPi version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='openmtpk',
-    version='0.0.12',
+    version='0.3.3',
     author='Akiel Aries',
     author_email='akiel@akiel.org',
     description='openMTPK Python API',
