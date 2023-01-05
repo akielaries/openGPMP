3f5c94a18763e3fe7533ed02767383679805afb5
montaquila <akiel@akiel.org>
2023-01-05 14:47:37 -0700

    testing Python PIP installation with PyPi

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='openmtpk',
-    version='0.0.8',
+    version='0.0.9',
     author='Akiel Aries',
     author_email='akiel@akiel.org',
     description='openMTPK Python API',
