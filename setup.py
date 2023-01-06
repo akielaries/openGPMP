01aeafe9f14a76af4b4fb833359fe2b851fc9a98
montaquila <akiel@akiel.org>
2023-01-05 21:44:01 -0700

    testing builds for cpp and python on linux and macos

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='openmtpk',
-    version='0.3.3',
+    version='0.4.0',
     author='Akiel Aries',
     author_email='akiel@akiel.org',
     description='openMTPK Python API',
