289a432447ae413f80656048886ece3ce64cc58d
Akiel Aries <akiel@akiel.org>
2023-01-06 00:48:41 -0700

    testing OSX build of openMTPK core and Python

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='openmtpk',
-    version='0.4.0',
+    version='0.4.1',
     author='Akiel Aries',
     author_email='akiel@akiel.org',
     description='openMTPK Python API',
