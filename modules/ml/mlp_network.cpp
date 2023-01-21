7e98baa6618f037bf660a7a5a6bc24bebc6ef8dc
montaquila <akiel@akiel.org>
2023-01-21 06:01:53 -0700

    updating constructor and deconstructor of PrimaryMLP class. will finish out implementing definitions in the header

--- modules/ml/mlp_network.cpp
+++ modules/ml/mlp_network.cpp
@@ -27,7 +27,7 @@ long double mtpk::ml::PrimaryMLP::rand_real(long double low,
     return ((long double) rand() / RAND_MAX) * (hi - low) + low;
 }
 
-/* MLP CONSTRUCTOR */
+/* PRIMARY MLP CONSTRUCTOR */
 mtpk::ml::PrimaryMLP::PrimaryMLP(int64_t nl, int64_t npl[]) : 
     num_layers(0), 
     layer_ptr(0),
@@ -38,13 +38,82 @@ mtpk::ml::PrimaryMLP::PrimaryMLP(int64_t nl, int64_t npl[]) :
     _MAE(0.0),
     _AvgTestError(0.0) { 
     
-    int64_t i, j;
+    int64_t _LAYER, _NEURON;
 
     // create network layers
     num_layers = nl;
     layer_ptr = new layer[nl];
 
+    // intialize the data of the created network layers
+    for(_LAYER = 0; _LAYER < nl; _LAYER++) {
+        // intialize values to neuron struct information
+        layer_ptr[_LAYER].num_neurons = npl[_LAYER];
+        layer_ptr[_LAYER].neuron_ptr = new neuron[npl[_LAYER]];
 
+        // intialize date of the neurons of the created network layers
+        for(_NEURON = 0; _NEURON < npl[_LAYER]; _NEURON++) {
+            // initialize exit value
+            layer_ptr[_LAYER].neuron_ptr[_NEURON].sortir = 1.0;
+            // save the error
+            layer_ptr[_LAYER].neuron_ptr[_NEURON].err = 0.0;
+            
+            // check if there is at least 1 layer
+            if(_LAYER > 0) {
+                /* initialize weight, last weight, and saved weight
+                 * values to _LAYER - 1
+                 */
+                layer_ptr[_LAYER].neuron_ptr[_NEURON].wt = 
+                    new long double[npl[_LAYER - 1]];
+
+                layer_ptr[_LAYER].neuron_ptr[_NEURON].wt_last =
+                    new long double[npl[_LAYER - 1]];
+
+                layer_ptr[_LAYER].neuron_ptr[_NEURON].wt_saved =
+                    new long double[npl[_LAYER - 1]];
+            }
+            // otherwise
+            else {
+                /*
+                 * initialize weight, last weight, and saved weight 
+                 * to NULL
+                 */
+                layer_ptr[_LAYER].neuron_ptr[_NEURON].wt = NULL;
+                layer_ptr[_LAYER].neuron_ptr[_NEURON].wt_last = NULL;
+                layer_ptr[_LAYER].neuron_ptr[_NEURON].wt_saved = NULL;
+            }
+        }
+    }
+}
+
+/* PRIMARY MLP DECONSTRUCTOR */
+mtpk::ml::PrimaryMLP::~PrimaryMLP() {
+    int64_t _LAYER, _NEURON;
+
+    // TODO : thread the loops dealing with rudimentary computations
+
+    for(_LAYER = 0; _LAYER < num_layers; _LAYER++) {
+        if (layer_ptr[_LAYER].neuron_ptr) {   
+            
+            for(_NEURON = 0; 
+                _NEURON < layer_ptr[_LAYER].num_neurons; 
+                _NEURON++) {
+            
+                if (layer_ptr[_LAYER].neuron_ptr[_NEURON].wt) {
+                    delete[] layer_ptr[_LAYER].neuron_ptr[_NEURON].wt;
+                }
+                
+                if (layer_ptr[_LAYER].neuron_ptr[_NEURON].wt_last) {
+                    delete[] layer_ptr[_LAYER].neuron_ptr[_NEURON].wt_last;
+                }
+
+                if (layer_ptr[_LAYER].neuron_ptr[_NEURON].wt_saved) {
+                    delete[] layer_ptr[_LAYER].neuron_ptr[_NEURON].wt_saved;
+                }
+            }
+        }   
+        delete[] layer_ptr[_LAYER].neuron_ptr;
+    }   
+    delete[] layer_ptr;
 }
 
 
