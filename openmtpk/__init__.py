30b3371c3df4f55f19db5eb54619bc2600bbebbc
montaquila <akiel@akiel.org>
2023-01-26 12:44:27 -0700

    #FIX: running autopep8 for python formatting

--- openmtpk/__init__.py
+++ openmtpk/__init__.py
@@ -2,16 +2,16 @@
 openMTPK
 ========
 Python API for C++ based Mathematics Package. See the following links for
-details and documentation: 
+details and documentation:
     SOURCE CODE : https://github.com/akielaries/openMTPK/
     DOCS        : https://akielaries.github.io/openMTPK/
     PyPI        : https://pypi.org/project/openmtpk/
 
 """
 
+from . import _version
 __all__: list[str]
 __path__: list[str]
 
 
-from . import _version
 __version__ = _version.get_versions()['version']
