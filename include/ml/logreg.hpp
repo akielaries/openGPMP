7ee24a9edd2242011f0eba2db805b70c0555732c
montaquila <akiel@akiel.org>
2023-02-12 01:26:13 -0700

    #EDIT: updating comments prepended to files and workflow

--- include/ml/logreg.hpp
+++ include/ml/logreg.hpp
@@ -1,3 +1,37 @@
+/*************************************************************************
+ *                                  
+ *  Project               
+ *                        __  __ _______ _____  _  __
+ *                       |  \/  |__   __|  __ \| |/ /
+ *  ___  _ __   ___ _ __ | \  / |  | |  | |__) | ' / 
+ * / _ \| '_ \ / _ \ '_ \| |\/| |  | |  |  ___/|  <  
+ *| (_) | |_) |  __/ | | | |  | |  | |  | |    | . \ 
+ * \___/| .__/ \___|_| |_|_|  |_|  |_|  |_|    |_|\_\  
+ *      | |                                          
+ *      |_|                                         
+ *
+ *
+ * Copyright (C) Akiel Aries, <akiel@akiel.org>, et al.
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution. The terms
+ * among other details are referenced in the official documentation
+ * seen here : https://akielaries.github.io/openMTPK/ along with 
+ * important files seen in this project.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute 
+ * and/or sell copies of the Software, and permit persons to whom 
+ * the Software is furnished to do so, under the terms of the 
+ * LICENSE file. As this is an Open Source effort, all implementations
+ * must be of the same methodology.
+ *
+ *
+ *
+ * This software is distributed on an AS IS basis, WITHOUT 
+ * WARRANTY OF ANY KIND, either express or implied.
+ *
+ ************************************************************************/
+
 /**
  * @files
  *
