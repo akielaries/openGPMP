ebf9997619effeb603f895ceb2c5266467233c93
Akiel Aries <56521583+akielaries@users.noreply.github.com>
2023-02-16 00:04:08 -0700

    Delete configure.sh

--- scripts/configure.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-# configuration script for github actions
-CXX=g++          # The C++ compiler
-NATIVE=on        # compiles code targeted to current hardware
-TUNE=generic     # performance-tuning switch
-
