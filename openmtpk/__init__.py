181d840c6160b2fa1f884080c858202380e4d95e
montaquila <akiel@akiel.org>
2023-04-11 00:38:07 -0700

    #EDITS: updates to documentation

--- openmtpk/__init__.py
+++ openmtpk/__init__.py
@@ -1,10 +1,10 @@
-"""
-@brief Python API for openMTPK. See the following links for
-details and documentation:
+"""!@brief Python API for openMTPK. 
+@details See the following links for details and documentation:
     SOURCE CODE : https://github.com/akielaries/openMTPK/
     DOCS        : https://akielaries.github.io/openMTPK/
     PyPI        : https://pypi.org/project/openmtpk/
-
+"""
+"""
 /*************************************************************************
  *
  *  Project
