30e78b15aa686a1d9dc95becc0190822308a1fb2
montaquila <akiel@akiel.org>
2023-04-12 05:48:56 -0700

    #EDITS: updates to datatable class. everything works as expected if the specified columns are passed in as they appear in the file being read

--- include/core/datatable.hpp
+++ include/core/datatable.hpp
@@ -34,7 +34,7 @@
 
 /*
  * The datatable class similar to the Pandas Dataframe, R data.table, etc
- * with a bit more limitations.
+ * with many more limitations.
  */
 #ifndef DATATABLE_HPP
 #define DATATABLE_HPP
@@ -49,11 +49,107 @@
 
 namespace mtpk {
 
+// alias for the pair type of strings
+typedef std::pair<std::vector<std::string>,
+                  std::vector<std::vector<std::string>>>
+    DataTableStr;
+// alias for pair type of 64 bit integers
+typedef std::pair<std::vector<int64_t>, std::vector<std::vector<int64_t>>>
+    DataTableInt;
+
+// alias for pair type of long doubles
+typedef std::pair<std::vector<long double>,
+                  std::vector<std::vector<long double>>>
+    DataTableDouble;
+
+
 class DataTable {
   public:
-    std::pair<std::vector<std::string>, std::vector<std::vector<std::string>>>
-    readCSV(std::string filename, std::vector<std::string> columns = {});
+    /**
+     * @brief Reads a CSV file and returns a DataTableStr.
+     * parses CSV files and stores all data as strings
+     * @param filename the path to the CSV file
+     * @param columns optional vector of column names to read in, if empty all
+     * columns will be read in
+     * @return a DataTableStr containing the column names and data
+     */
+    DataTableStr csv_read(std::string filename,
+                          std::vector<std::string> columns = {});
+
+    // converts a type DataTableStr to DataTableInt
+    /**
+     * @brief Converts a DataTableStr to a DataTableInt
+     * @param src The DataTableStr to convert
+     * @return The converted DataTableInt
+     * @note This function assumes that the input DataTableStr contains only
+     * elements that can be converted to a 64-bit integer using std::stoi().
+     * @TODO allow for specific columns to be converted 
+     * @TODO make use of ThreadPool
+     */
+    DataTableInt str_to_int(DataTableStr src);
+
+    /**
+     * @brief Converts a DataTableStr to a DataTableDouble
+     * @param src A DataTableStr object to be converted.
+     * @return The converted DataTableDouble.
+     * @note The function assumes the input DataTableStr contains only valid
+     * double type elements
+     */
+    DataTableDouble str_to_double(DataTableStr src);
+
+    /**
+     * @brief Displays a DataTable of type T with the option to display all or a
+     * subset of rows
+     * @tparam T The type of the DataTable to be displayed
+     * @param data A pair of vectors representing the header and data rows of
+     * the DataTable
+     * @param display_all A flag indicating whether to display all rows or just
+     * a subset
+     */
+    template <typename T>
+    void display(std::pair<std::vector<T>, std::vector<std::vector<T>>> data,
+                 bool display_all = false) {
+        int num_columns = data.first.size();
+        int num_rows = data.second.size();
+        int num_omitted_rows = 0;
+
+        for (const auto &header : data.first) {
+            std::cout << header << "\t";
+        }
+        std::cout << std::endl;
+
+        int num_elements = data.second.size();
+        if (!display_all && num_elements > 10) {
+            for (int i = 0; i < 5; i++) {
+                for (const auto &value : data.second[i]) {
+                    std::cout << value << "\t";
+                }
+                std::cout << std::endl;
+            }
+            num_omitted_rows = num_elements - 10;
+            std::cout << "...\n";
+            std::cout << "[" << num_omitted_rows << " rows omitted]\n";
+            for (int i = num_elements - 5; i < num_elements; i++) {
+                for (const auto &value : data.second[i]) {
+                    std::cout << value << "\t";
+                }
+                std::cout << std::endl;
+            }
+        } else {
+            for (const auto &row : data.second) {
+                for (const auto &value : row) {
+                    std::cout << value << "\t";
+                }
+                std::cout << std::endl;
+            }
+        }
+
+        std::cout << "[" << num_rows << " rows"
+                  << " x " << num_columns << " columns";
+        std::cout << "]\n\n";
+    }
 };
 
 } // namespace mtpk
+
 #endif // DATATABLE_HPP
--- modules/core/datatable.cpp
+++ modules/core/datatable.cpp
@@ -3,14 +3,13 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <regex>
 #include <sstream>
 #include <string>
 #include <vector>
 
-std::pair<std::vector<std::string>, std::vector<std::vector<std::string>>>
-mtpk::DataTable::readCSV(std::string filename,
-                         std::vector<std::string> columns) {
-
+mtpk::DataTableStr mtpk::DataTable::csv_read(std::string filename,
+                                             std::vector<std::string> columns) {
     std::ifstream file(filename);
 
     if (!file.is_open()) {
@@ -69,4 +68,48 @@ mtpk::DataTable::readCSV(std::string filename,
 
     file.close();
     return make_pair(columns, data);
+}
+
+mtpk::DataTableInt mtpk::DataTable::str_to_int(mtpk::DataTableStr src) {
+    mtpk::DataTableInt dest;
+
+    for (const auto &v : src.first) {
+        if (std::regex_match(
+                v, std::regex("\\d+"))) { // check if v contains only digits
+            dest.first.push_back(std::stoi(v));
+        }
+    }
+    for (const auto &vv : src.second) {
+        std::vector<int64_t> new_vec;
+        for (const auto &v : vv) {
+            if (std::regex_match(
+                    v, std::regex("\\d+"))) { // check if v contains only digits
+                new_vec.push_back(std::stoi(v));
+            }
+        }
+        dest.second.push_back(new_vec);
+    }
+    return dest;
+}
+
+mtpk::DataTableDouble mtpk::DataTable::str_to_double(mtpk::DataTableStr src) {
+    mtpk::DataTableDouble dest;
+
+    for (const auto &v : src.first) {
+        if (std::regex_match(v, std::regex("[-+]?\\d*\\.?\\d+"))) {
+            dest.first.push_back(std::stold(v));
+        }
+    }
+
+    for (const auto &vv : src.second) {
+        std::vector<long double> new_vec;
+        for (const auto &v : vv) {
+            if (std::regex_match(v, std::regex("[-+]?\\d*\\.?\\d+"))) {
+                new_vec.push_back(std::stold(v));
+            }
+        }
+        dest.second.push_back(new_vec);
+    }
+
+    return dest;
 }
