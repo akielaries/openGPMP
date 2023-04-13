72eb1e416dd10890df2d065df72ca9c70786b1b4
akielaries <56521583+akielaries@users.noreply.github.com>
2023-04-13 00:58:03 +0000

    Deploying to gh-pages from  @ 1d861c087188ce76911b45fc05e79bfa1000738e 🚀

--- search/typedefs_1.js
+++ search/typedefs_1.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['datatabledouble_1073',['DataTableDouble',['../namespacemtpk.html#afc4e72528c63d7a7cf8f85599ea2ede0',1,'mtpk']]],
-  ['datatableint_1074',['DataTableInt',['../namespacemtpk.html#a07a1ee36e02865a60adae5a5fe8fff1e',1,'mtpk']]],
-  ['datatablestr_1075',['DataTableStr',['../namespacemtpk.html#a9b115dee30a56ad053767b46f7bf016b',1,'mtpk']]]
+  ['colvector_926',['ColVector',['../naive__net_8hpp.html#a9f8f99187c9577dde49b0c3491137d2f',1,'naive_net.hpp']]]
 ];
