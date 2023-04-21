d0c5c1f2506ed7012ec2a52c28ac9f1c267e6a93
montaquila <akiel@akiel.org>
2023-04-21 03:48:44 -0700

    #EDITS: need to finish updating file names and structure for core module

---
+++ include/core.hpp
@@ -0,0 +1,19 @@
+/**
+ * openMTPK core module TODO FIXME
+ */
+
+/*
+ * ifndef if certain hardware accel tools are included in CMake install
+ * process 
+ *
+ *
+ * threadpool
+ * datatable
+ * utils
+ *
+ * OPTIONAL:
+ * opencl GPU
+ *
+ * SIMD
+ */
+
