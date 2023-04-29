b1334b00c62a8376f8376b086edefd86ddc04fda
Akiel Aries <56521583+akielaries@users.noreply.github.com>
2023-04-29 15:34:57 -0700

    #MERGE: Add DataTable class for issue #29 (#53)
    * #EDITS: adding DataTable class for issue #29
    
    * #EDITS: adding macros for rows to show and max rows

---
+++ modules/core/datatable.cpp
@@ -0,0 +1,151 @@
+/*************************************************************************
+ *
+ *  Project
+ *                        __  __ _______ _____  _  __
+ *                       |  \/  |__   __|  __ \| |/ /
+ *  ___  _ __   ___ _ __ | \  / |  | |  | |__) | ' /
+ * / _ \| '_ \ / _ \ '_ \| |\/| |  | |  |  ___/|  <
+ *| (_) | |_) |  __/ | | | |  | |  | |  | |    | . \
+ * \___/| .__/ \___|_| |_|_|  |_|  |_|  |_|    |_|\_\
+ *      | |
+ *      |_|
+ *
+ *
+ * Copyright (C) Akiel Aries, <akiel@akiel.org>, et al.
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution. The terms
+ * among other details are referenced in the official documentation
+ * seen here : https://akielaries.github.io/openMTPK/ along with
+ * important files seen in this project.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute
+ * and/or sell copies of the Software, and permit persons to whom
+ * the Software is furnished to do so, under the terms of the
+ * LICENSE file. As this is an Open Source effort, all implementations
+ * must be of the same methodology.
+ *
+ *
+ *
+ * This software is distributed on an AS IS basis, WITHOUT
+ * WARRANTY OF ANY KIND, either express or implied.
+ *
+ ************************************************************************/
+#include "../../include/core/datatable.hpp"
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <vector>
+
+mtpk::core::DataTableStr
+mtpk::core::DataTable::csv_read(std::string filename,
+                                std::vector<std::string> columns) {
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Unable to open file: " << filename << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::vector<std::vector<std::string>> data;
+    std::string line;
+
+    // Get the header line and parse the column names
+    getline(file, line);
+    std::stringstream header(line);
+    std::vector<std::string> headerColumns;
+    std::string columnName;
+
+    while (getline(header, columnName, ',')) {
+        headerColumns.push_back(columnName);
+    }
+
+    // If no columns are specified, read in all columns
+    if (columns.empty()) {
+        columns = headerColumns;
+    }
+
+    // Check if specified columns exist in the header
+    for (const auto &column : columns) {
+        if (find(headerColumns.begin(), headerColumns.end(), column) ==
+            headerColumns.end()) {
+            std::cerr << "Column not found: " << column << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Read in the data rows
+    while (getline(file, line)) {
+        std::vector<std::string> row;
+        std::stringstream rowStream(line);
+        std::string value;
+        int columnIndex = 0;
+
+        while (getline(rowStream, value, ',')) {
+            // If column is specified, only read in specified columns
+            if (find(columns.begin(), columns.end(),
+                     headerColumns[columnIndex]) != columns.end()) {
+                row.push_back(value);
+            }
+
+            columnIndex++;
+        }
+
+        if (row.size() > 0) {
+            data.push_back(row);
+        }
+    }
+
+    file.close();
+    return make_pair(columns, data);
+}
+
+mtpk::core::DataTableInt
+mtpk::core::DataTable::str_to_int(mtpk::core::DataTableStr src) {
+    mtpk::core::DataTableInt dest;
+
+    for (const auto &v : src.first) {
+        // check if v contains only digits
+        if (std::regex_match(v, std::regex("\\d+"))) {
+            dest.first.push_back(std::stoi(v));
+        }
+    }
+    for (const auto &vv : src.second) {
+        std::vector<int64_t> new_vec;
+        for (const auto &v : vv) {
+            // check if v contains only digits
+            if (std::regex_match(v, std::regex("\\d+"))) {
+                new_vec.push_back(std::stoi(v));
+            }
+        }
+        dest.second.push_back(new_vec);
+    }
+    return dest;
+}
+
+mtpk::core::DataTableDouble
+mtpk::core::DataTable::str_to_double(mtpk::core::DataTableStr src) {
+    mtpk::core::DataTableDouble dest;
+
+    for (const auto &v : src.first) {
+        if (std::regex_match(v, std::regex("[-+]?\\d*\\.?\\d+"))) {
+            dest.first.push_back(std::stold(v));
+        }
+    }
+
+    for (const auto &vv : src.second) {
+        std::vector<long double> new_vec;
+        for (const auto &v : vv) {
+            if (std::regex_match(v, std::regex("[-+]?\\d*\\.?\\d+"))) {
+                new_vec.push_back(std::stold(v));
+            }
+        }
+        dest.second.push_back(new_vec);
+    }
+
+    return dest;
+}
